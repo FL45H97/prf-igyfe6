896af81942aad88260d8d9ba21302269294c2610
Papdi Pál Soma <somavanpapdiv@gmail.com>
2021-05-23 16:05:57 +0200

    clean

--- target/classes/init.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS products;
-CREATE TABLE products(id serial PRIMARY KEY, name VARCHAR(255), description VARCHAR(255), price INTEGER, quantity INTEGER);
-DROP SEQUENCE IF EXISTS hibernate_sequence;
-CREATE SEQUENCE hibernate_sequence START 1;
